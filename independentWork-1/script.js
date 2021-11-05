ea0600bf5635ee0d27ac9511ffca21a61f36b52d
kirill <kirill.trofim@gmail.com>
2021-11-05 12:33:03 +0200

    FirstTry

---
+++ independentWork-1/script.js
@@ -0,0 +1,45 @@
+alert('Hello World');
+
+let q = {
+    name: 'Kirill',
+    surname: 'Trofim',
+    age: 20
+};
+console.log(typeof q);
+alert(q);
+
+let a = 2.4;
+console.log(Math.floor(a)); // 2 округление
+console.log(Math.round(a)); // 2 округление в > сторону
+console.log(Math.ceil(a)); // 3 округление в < сторону
+
+let b = 'Мне 20 лет';
+console.log(parseInt(b));   // Выводит NaN = ?
+
+let w = [1,2,4,5,6,7];
+w = Math.max(w);
+console.log(w);  // Так же NaN = ?
+
+alert (Math.max (3,6));
+alert (Math.max (w));
+
+let str = 'Hello world';
+console.log(str.indexOf('world')); // 6
+console.log(str.indexOf('World')); // -1
+console.log(str.includes('wo')); // true
+console.log(str.includes('Wo')); // false
+
+let check = prompt('Префикс или постфикс?', ' ');
+let counter = 1;
+let result;
+while ( check == ' ' || check == undefined ){
+    check = prompt('Префикс или постфикс?', ' ');
+}
+
+if (check.toUpperCase()=='ПОСТФИКС') {
+    result=counter++; // сначала выдает result, а затем прибавляет единицу, r=1
+} else if (check.toUpperCase()=='ПРЕФИКС'){
+    result=++counter; // сначала прибавляет единицу, а затем выдает result, r=2
+};
+console.log(result);
+
