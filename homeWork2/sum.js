f8e3306447c772bf17809bde791982250ede77e5
kirilltrofim <kirill.trofim@gmail.com>
2021-11-11 15:46:24 +0200

    edit sum

--- homeWork2/sum.js
+++ homeWork2/sum.js
@@ -1,17 +1,27 @@
+// export const sum = (f, s) => {
+//   //TODO: Должны складываться только строки и числа
+//   // Постарайтесь сложить как можно больше пар и обойти NaN случаи
+//   let sum;
+//   if (typeof f == "number" && typeof s == "number") return f + s;
+//   else if (typeof f == "number" && typeof s == "string") {
+//     s = parseFloat(s);
+//     return f + s;
+//   } else if (typeof f == "number" && typeof s == "object") return f;
+//   else if (typeof f == "boolean" && typeof s == "string") return +f;
+//   // мало времени не успеваю написать не привязываясь к данным
+//   else if (typeof f == "string" && typeof s == "number")
+//     if (isNaN(parseInt(f))) return s;
+//     else return parseInt(f) + s;
+
+//   return sum;
+// };
+
 export const sum = (f, s) => {
   //TODO: Должны складываться только строки и числа
   // Постарайтесь сложить как можно больше пар и обойти NaN случаи
-  let sum;
-  if (typeof f == "number" && typeof s == "number") return f + s;
-  else if (typeof f == "number" && typeof s == "string") {
-    s = parseFloat(s);
-    return f + s;
-  } else if (typeof f == "number" && typeof s == "object") return f;
-  else if (typeof f == "boolean" && typeof s == "string") return +f;
-  // мало времени не успеваю написать не привязываясь к данным
-  else if (typeof f == "string" && typeof s == "number")
-    if (isNaN(parseInt(f))) return s;
-    else return parseInt(f) + s;
-
-  return sum;
+  if (isNaN(parseInt(f))) {
+    if (isNaN(parseInt(s))) return 0;
+    else return s;
+  } else if (isNaN(parseInt(s))) return f;
+  else return parseInt(f) + parseInt(s);
 };
