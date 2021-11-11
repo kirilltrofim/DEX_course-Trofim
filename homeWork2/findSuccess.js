78c8326cdd94d1dffb7fd19bfb1968dc7201dea7
kirilltrofim <kirill.trofim@gmail.com>
2021-11-11 09:53:30 +0200

    findSuccess_+-ready

--- homeWork2/findSuccess.js
+++ homeWork2/findSuccess.js
@@ -1,6 +1,17 @@
-export const findSuccess = (bankResponse) => {
-    //TODO: Проверьте что в строке есть 'Success' без учёта регистра
-    let regexp = /Success/gi;
-    let reg = new RegExp("Success", "i");
-    return bankResponse === bankResponse.match("Success") ? "Yes" : "No";
+// export const findSuccess = (bankResponse) => {
+//     //TODO: Проверьте что в строке есть 'Success' без учёта регистра
+//     let regexp = /Success/gi;
+//     let reg = new RegExp("Success", "i");
+//     return bankResponse === bankResponse.match("Success") ? "Yes" : "No";
+//   };
+
+  const findSuccess = (bankResponse) => {
+
+    if(bankResponse.split(" ").map(item => item.toLowerCase()).find(item => item === "success") !== undefined)
+        return "Yes";
+      else
+        if(bankResponse.split(/(?=[A-Z])/).map(item => item.toLowerCase()).find(item => item === "success"))
+            return "Yes";
+          else
+              return "No";
   };
