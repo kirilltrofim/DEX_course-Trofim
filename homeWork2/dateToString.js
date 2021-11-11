8721cf5f2a03df811d10343175976e360ca8c784
kirilltrofim <kirill.trofim@gmail.com>
2021-11-11 15:00:18 +0200

    edit dataToString

--- homeWork2/dateToString.js
+++ homeWork2/dateToString.js
@@ -1,22 +1,29 @@
-const formatDate = (date) => {
+export const dateToString = (date) => {
+
+  const formatDate = (date) => {
     let a = new Date(date).toISOString().split("T");
     return a[0];
   };
 
-  const transformationData = (num) =>{
+  const transformationData = (num) => {
     return new Date(Date.now() + num).toISOString().split("T")[0];
   };
 
-  const dateToString = (date) => {
-    let yesterday = transformationData(-86400000);
-    let today = transformationData(0);
-    let tomorrow = transformationData(86400000);
-    let checkedDate = formatDate(date);
+  let yesterday = transformationData(-86400000);
+  let today = transformationData(0);
+  let tomorrow = transformationData(86400000);
+  let checkedDate = formatDate(date);
 
-    switch (checkedDate){
-      case yesterday: return "Вчера"; break;
-      case tomorrow: return "Завтра"; break;
-      case today: return "Сегодня"; break;
-    }
-    return checkedDate;
-  };
+  switch (checkedDate) {
+    case yesterday:
+      return "Вчера";
+      break;
+    case tomorrow:
+      return "Завтра";
+      break;
+    case today:
+      return "Сегодня";
+      break;
+  }
+  return checkedDate;
+};
