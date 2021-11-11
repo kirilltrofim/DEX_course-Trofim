d438a0a384359b0464065471793ee024299e0045
kirilltrofim <kirill.trofim@gmail.com>
2021-11-11 14:17:16 +0200

    dataToString_ready

---
+++ homeWork2/dateToString.js
@@ -0,0 +1,22 @@
+const formatDate = (date) => {
+    let a = new Date(date).toISOString().split("T");
+    return a[0];
+  };
+
+  const transformationData = (num) =>{
+    return new Date(Date.now() + num).toISOString().split("T")[0];
+  };
+
+  const dateToString = (date) => {
+    let yesterday = transformationData(-86400000);
+    let today = transformationData(0);
+    let tomorrow = transformationData(86400000);
+    let checkedDate = formatDate(date);
+
+    switch (checkedDate){
+      case yesterday: return "Вчера"; break;
+      case tomorrow: return "Завтра"; break;
+      case today: return "Сегодня"; break;
+    }
+    return checkedDate;
+  };
