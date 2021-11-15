f94dea8bcf7225182c5c413b0bb85694c49a5665
kirill <kirill.trofim@gmail.com>
2021-11-15 16:26:29 +0200

    repeatOrder.js

---
+++ homeWork3/repeatOrder.js
@@ -0,0 +1,13 @@
+export const repeatOrder = (cartData, date) => {
+    //TODO: нужно повторить заказ за выбранную дату
+    // дублиовать соответствующие элементы
+    // поставить в начало спиcка
+    // дату текущую
+    // поменять id на уникальный
+    const now = new Date().toISOString();
+    const filterCartData = cartData.filter(item => item.date === date).map((item,i) => item = {...item, id:String(cartData.length + i), date:now});
+
+    cartData = [...filterCartData, ...cartData];
+    console.log(cartData);
+    return cartData;
+  };
