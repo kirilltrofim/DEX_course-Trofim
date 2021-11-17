eb07624f4d22113b7bb569c89dbcecbe05a2b20b
kirill <kirill.trofim@gmail.com>
2021-11-17 20:01:39 +0200

    add logger

--- homeWork4/workHere.js
+++ homeWork4/workHere.js
@@ -1,19 +1,35 @@
 //TODO: Напишите функцию счётчик вызовов, должна принимать название и функцию, а возвращать функцию
 // Выводит в консоль, в момент вызова функции из параметров, кол-во вызовов
-export const counter = () => {
+export const counter = (name, func) => {
   let num = 0;
   return function () {
     num++;
-    console.log(num)
-    return counter();
+    console.log(num);
+    return func;
   };
 };
 
 //TODO: Напишите функцию логгер, должна принимать название и функцию, а возвращать функцию
 // Выводит в консоль, в момент вызова функции из параметров, её параметры результат и название
-export const logger = () => {};
+export const logger = (name, fn) => {
+  return function (...args) {
+    this.fn = fn;
+    console.log(
+      ` параметры: ${args} \n результат:${this.fn(args)} \n название:${name}`
+    );
+    return this.fn;
+  };
+};
 
 //TODO: Напишите функцию каррирования, должна принимать функцию, а возвращать функцию
 // Каррирование – это трансформация функций таким образом, чтобы они принимали аргументы не как f(a, b, c), а как f(a)(b)(c)
 // Должна работать для любого кол-ва аргументов
-export const curry = () => {};
+export const curry = (func) => {
+  return function curried(...args) {
+    if (args.length >= func.length) return func.apply(this, args);
+    else
+      return function (...args2) {
+        curried.apply(this, args.concat(args2));
+      };
+  };
+};
