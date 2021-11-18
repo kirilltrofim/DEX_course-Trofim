96e24c84cd0ab2b07f33bdf5f563c4fa0415573b
kirilltrofim <kirill.trofim@gmail.com>
2021-11-18 15:51:29 +0200

    edit curry

--- homeWork4/workHere.js
+++ homeWork4/workHere.js
@@ -22,7 +22,32 @@ export const logger = (name, fn) => {
 //TODO: Напишите функцию каррирования, должна принимать функцию, а возвращать функцию
 // Каррирование – это трансформация функций таким образом, чтобы они принимали аргументы не как f(a, b, c), а как f(a)(b)(c)
 // Должна работать для любого кол-ва аргументов
-<<<<<<< HEAD
+//TODO: Напишите функцию счётчик вызовов, должна принимать название и функцию, а возвращать функцию
+// Выводит в консоль, в момент вызова функции из параметров, кол-во вызовов
+export const counter = (name, func) => {
+  let num = 0;
+  return function () {
+    num++;
+    console.log(num);
+    return func;
+  };
+};
+
+//TODO: Напишите функцию логгер, должна принимать название и функцию, а возвращать функцию
+// Выводит в консоль, в момент вызова функции из параметров, её параметры результат и название
+export const logger = (name, fn) => {
+  return function (...args) {
+    this.fn = fn;
+    console.log(
+      ` параметры: ${args} \n результат:${this.fn(args)} \n название:${name}`
+    );
+    return this.fn;
+  };
+};
+
+//TODO: Напишите функцию каррирования, должна принимать функцию, а возвращать функцию
+// Каррирование – это трансформация функций таким образом, чтобы они принимали аргументы не как f(a, b, c), а как f(a)(b)(c)
+// Должна работать для любого кол-ва аргументов
 export const curry = (func) => {
   return function curried(...rest) {
     let this1 = this;
@@ -30,16 +55,6 @@ export const curry = (func) => {
     else
       return function (...rest2) {
         return curried.apply(this1, rest.concat(rest2));
-=======
-export const curry = (f) => {
-  return function func(...args) {
-    if (args.length >= f.length) {
-      return f.apply(this, args);
-    } else {
-      return function (...args2) {
-        return func.apply(this, args.concat(args2));
->>>>>>> master
       };
-    }
   };
 };
