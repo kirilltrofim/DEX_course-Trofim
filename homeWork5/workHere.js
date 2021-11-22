ee4e7729a5bf5b06f621605fdbf37e2020ebabe6
kirill <kirill.trofim@gmail.com>
2021-11-22 11:06:07 +0200

    add getResponse

--- homeWork5/workHere.js
+++ homeWork5/workHere.js
@@ -21,3 +21,33 @@ const processingData = async (str) => {
 };
 processingData("короткий текст");
 processingData("длинный тексттттттттттт");
+
+// Используя конструкции then catch
+// к положительному ответу добавьте " :)"
+// к ответу с ошибкой добавьте " :("
+// Если длина ответа меньше 20 символов, то нужно добавить "(" или ")" в зависимости от ответа,
+// скобочки нужно добавлять пока длинна не станет равна 20
+// TODO: на каждое действи должна быть отдельная конструкция then или catch
+// Например первый then для добавления " :)", второй для подсчёта количества символов и добавления недостающих
+const getResponse = async (str) => {
+  compareText(str)
+    .then((result) => result + " :)")
+    .catch((error) => {
+      throw new Error(error.message + " :(");
+    })
+    .then((result) => {
+      while (result.length <= 20) {
+        result = result + ")";
+      }
+      console.log(result);
+    })
+    .catch((error) => {
+      while (error.message.length <= 20) {
+        error.message = error.message + "(";
+      }
+      console.error(error.message);
+    });
+};
+
+getResponse("короткий текст");
+getResponse("Длинный текстттт");
